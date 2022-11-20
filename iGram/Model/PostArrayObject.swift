a28b8e373c4534cedec9f08bf947c15c1dbdcb73
Alexandr_Kozin <sasha_kozin2020@mail.ru>
2022-11-21 00:41:17 +0300

    Setting up a data structure for posts (PostModel)

---
+++ iGram/Model/PostArrayObject.swift
@@ -0,0 +1,28 @@
+//
+//  PostArrayObject.swift
+//  iGram
+//
+//  Created by Alexandr Kozin on 20.11.2022.
+//
+
+import Foundation
+
+class PostArrayObject: ObservableObject {
+    
+    @Published var dataArray = [PostModel]()
+    
+    init() {
+        
+        print("FETCH FROM DATABASE HERE")
+        
+        let post1 = PostModel(postID: "", userID: "", username: "Alex Kozin", caption: "This is a caption", dateCreated: Date(), likeCount: 0, likedByUser: false)
+        let post2 = PostModel(postID: "", userID: "", username: "Alex Petrishchev", caption: "Biba", dateCreated: Date(), likeCount: 0, likedByUser: false)
+        let post3 = PostModel(postID: "", userID: "", username: "Nikita Simakov", caption: "Boba", dateCreated: Date(), likeCount: 0, likedByUser: false)
+        let post4 = PostModel(postID: "", userID: "", username: "Kostya Lishenkov", caption: "TFT the best", dateCreated: Date(), likeCount: 0, likedByUser: false)
+        
+        self.dataArray.append(post1)
+        self.dataArray.append(post2)
+        self.dataArray.append(post3)
+        self.dataArray.append(post4)
+    }
+}

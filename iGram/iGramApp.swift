8a638c4ba382ec724c6023db5583d7d542fbfa80
Alexandr_Kozin <sasha_kozin2020@mail.ru>
2022-11-20 21:56:26 +0300

    Initial Commit

---
+++ iGram/iGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGramApp.swift
+//  iGram
+//
+//  Created by Alexandr Kozin on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct iGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

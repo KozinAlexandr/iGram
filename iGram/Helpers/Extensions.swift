50e3c0a310a55b66828c454b3f7bf7ca405c9ac7
Alexandr_Kozin <sasha_kozin2020@mail.ru>
2022-11-20 22:58:23 +0300

    Adding the tab bar (TabView)

---
+++ iGram/Helpers/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  iGram
+//
+//  Created by Alexandr Kozin on 20.11.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct MyTheme {
+        static var purpleColor: Color {
+            return Color("ColorPurple")
+        }
+        
+        static var yellowColor: Color {
+            return Color("ColorYellow")
+        }
+        
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+    }
+}
---
+++ iGram/Views/Screens/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  iGram
+//
+//  Created by Alexandr Kozin on 20.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen 1")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            Text("Screen 2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("Screen 3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            Text("Screen 4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
+        .accentColor(Color.MyTheme.purpleColor)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

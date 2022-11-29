70ca8dd9185de86bca40290c00e4acc802db70c0
Alexandr_Kozin <sasha_kozin2020@mail.ru>
2022-11-30 01:36:32 +0300

    Adding the ability to share and report posts

--- iGram/Views/Subview/PostView.swift
+++ iGram/Views/Subview/PostView.swift
@@ -12,8 +12,16 @@ struct PostView: View {
     @State var post: PostModel
     @State var animateLike: Bool = false
     @State var addHeartAnimationToView: Bool
+    @State var showActionSheet: Bool = false
+    @State var actionSheetType: PostActionSheetOption = .general
+    @State var postImage: UIImage = UIImage(named: "dog1")!
     
     var showHeaderAndFooter: Bool
+    
+    enum PostActionSheetOption {
+        case general
+        case reporting
+    }
     
     var body: some View {
         VStack(alignment: .center, spacing: 0, content: {
@@ -39,8 +47,16 @@ struct PostView: View {
                     
                     Spacer()
                     
-                    Image(systemName: "ellipsis")
-                        .font(.headline)
+                    Button(action: {
+                        showActionSheet.toggle()
+                    }, label: {
+                        Image(systemName: "ellipsis")
+                            .font(.headline)
+                    })
+                    .accentColor(.primary)
+                    .actionSheet(isPresented: $showActionSheet, content: {
+                        getActionSheet()
+                    })
                     
                 }
                 .padding(.all, 6)
@@ -49,7 +65,7 @@ struct PostView: View {
             // MARK: IMAGE
             
             ZStack {
-                Image("dog1")
+                Image(uiImage: postImage)
                     .resizable()
                     .scaledToFit()
                 
@@ -87,9 +103,13 @@ struct PostView: View {
                                 .foregroundColor(.primary)
                         })
                     
-                    
-                    Image(systemName: "paperplane")
-                        .font(.title3)
+                    Button(action: {
+                        sharePost()
+                    }, label: {
+                        Image(systemName: "paperplane")
+                            .font(.title3)
+                    })
+                    .accentColor(.primary)
                     
                     Spacer()
                 })
@@ -127,6 +147,61 @@ struct PostView: View {
         // Update the local data
         let updatedPost = PostModel(postID: post.postID, userID: post.userID, username: post.username, caption: post.caption, dateCreated: post.dateCreated, likeCount: post.likeCount - 1, likedByUser: false)
         self.post = updatedPost
+    }
+    
+    func getActionSheet() -> ActionSheet {
+        
+        switch self.actionSheetType {
+        case .general:
+            return ActionSheet(title: Text("What would you like to do?"), message: nil, buttons: [
+                .destructive(Text("Report"), action: {
+                    
+                    self.actionSheetType = .reporting
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        self.showActionSheet.toggle()
+                    }
+                }),
+                
+                .default(Text("Learn more..."), action: {
+                    print("LEARN MORE PRESSED")
+                }),
+                
+                .cancel()
+            ])
+        case .reporting:
+            return ActionSheet(title: Text("Why are you reporting this post?"), message: nil, buttons: [
+                
+                .destructive(Text("This is inappropriate"), action: {
+                    reportPost(reason: "This is inappropriate")
+                }),
+                .destructive(Text("This is spam"), action: {
+                    reportPost(reason: "This is spam")
+                }),
+                .destructive(Text("It made me uncomfortable"), action: {
+                    reportPost(reason: "It made me uncomfortable")
+                }),
+                
+                .cancel({
+                    self.actionSheetType = .general
+                })
+            ])
+        }
+    }
+    
+    func reportPost(reason: String) {
+        print("REPORT POST NOW")
+    }
+    
+    func sharePost() {
+        
+        let message = "Check out this post on iGram!"
+        let image = postImage
+        let link = URL(string: "https://www.google.com")!
+        
+        let activityViewController = UIActivityViewController(activityItems: [message, image, link], applicationActivities: nil)
+        
+        let viewController = UIApplication.shared.windows.first?.rootViewController
+        viewController?.present(activityViewController, animated: true, completion: nil)
     }
     
 }
